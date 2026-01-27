4270c7c177fbc485c6ea8bd24929f0fe4d8090e6
Yohannes Getachew <demisgech@gmail.com>
2026-01-27 13:59:57 +0300

    Setup the backend with express

---
+++ packages/backend/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (request: Request, response: Response) => {
+  response.send({
+    message: "Hello world!",
+  });
+});
+
+const port = process.env.PORT ?? 3000;
+
+app.listen(port, () => {
+  console.log(`The web server is listening on http://localhost:${port}`);
+});
